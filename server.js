e45de2a2a658437b30f11f3103201d62bae8490e
Sachin Nimshan <sachinnimshan@gmail.com>
2021-06-09 21:24:14 +0530

    latest

--- server.js
+++ server.js
@@ -15,12 +15,12 @@ app.use(express.json());
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-if(process.env.NODE_ENV === "production"){
+
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-}
+
 app.use('/api/email', emailRouter);
 
 app.use('/', (req,res)=>{
