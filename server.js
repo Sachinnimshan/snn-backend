44eb1f3d90ec868c1f375bebc5c966d9bce202ff
Sachin Nimshan <sachinnimshan@gmail.com>
2021-06-09 17:48:52 +0530

    latest

--- server.js
+++ server.js
@@ -12,12 +12,15 @@ const PORT = process.env.SERVER_PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+if(process.env.NODE_ENV === "production"){
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
+}
 app.use('/api/email', emailRouter);
 
 app.use('/', (req,res)=>{
