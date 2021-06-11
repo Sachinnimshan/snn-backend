2f4edef4023db3852c4015ad04461a8204f40ce0
Sachin Nimshan <sachinnimshan@gmail.com>
2021-06-11 15:43:26 +0530

    latest

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ if(process.env.NODE_ENV === "production"){
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
-  }
+}
 
 app.use('/', (req,res)=>{
     res.send("Server is Ready");
